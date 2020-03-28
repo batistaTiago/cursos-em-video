45b9cf319ca78afa3b9799c2d23ee3261a31821c
AbilioN <netobalby@gmail.com>
2020-03-27 22:19:55 -0300

    implementado home admin

---
+++ resources/views/adminhome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admnistrador - Cursos Online</title>
+</head>
+<body>
+    <video src="./abella.mp4" width="400" height="400">
+        <source>
+    </video>
+
+</body>
+</html>
